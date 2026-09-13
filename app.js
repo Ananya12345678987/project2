0a4295e60880a39fac2d12b4493446f294741e8f
Ananya12345678987 <ananyarayanagoudra7@gmail.com>
2026-09-13 19:50:24 +0530

    prep for deployment

--- app.js
+++ app.js
@@ -17,20 +17,11 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-
-
-
-
-
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
-
-
 const dbUrl =  process.env.ATLASDB_URL;
-console.log("DB URL:", dbUrl);
 
 main()
 .then(() => {
@@ -51,37 +42,16 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
-// const store = MongoStore.create({
-//     mongoUrl:dbUrl,
-//     crypto:{
-//         secret:process.env.SECRET,
-//     },
-//     touchAfter: 24*3600,
-// });
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   collectionName: "sessions",
   touchAfter: 24 * 3600
 });
 
-
-
 store.on("error", () => {
     console.log("ERROR in MONGO SESSION STORE",err);
 });
 
-
-// const sessionOptions = {
-//     store,
-//     secret : "mysupersecretcode",
-//     resave: false,
-//     saveUninitialized:true,
-//     cookie:{
-//         expires:Date.now() +7 * 24 * 60 * 1000,
-//         maxAge:7 * 24 * 60 * 60 * 1000,
-//         httpOnly: true,
-//     },
-// };
 const sessionOptions = {
   store: store,
   secret: "mysupersecretcode",
@@ -89,24 +59,12 @@ const sessionOptions = {
   saveUninitialized: false
 };
 
-
-
-app.get("/", (req, res) => {
-    res.redirect("/login");
-});
-
-
-
-
-
-
 app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -118,19 +76,9 @@ app.use((req,res,next) => {
     next();  
 });
 
-
-// app.get("/demouser",async(req,res) => {
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username:"Delta-student"
-//     });    
-
-// let registeredUser = await User.register(fakeUser,"helloworld");
-// res.send(registeredUser);
-// });
-
-
-
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
 
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
@@ -140,7 +88,6 @@ app.use((req,res,next) => {
     next(new ExpressError(404,"Page not found!"));
 });
 
-
 app.use((err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
@@ -149,8 +96,6 @@ app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
     res.status(statusCode).render("error.ejs", { err });
 });
-
-console.log(process.env.ATLASDB_URL);
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
